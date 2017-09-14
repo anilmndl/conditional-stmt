4a1c061de31707572e598070918f0d0b1feb5304
Anil <anil_mndl@yahoo.com>
2017-09-13 18:05:03 -0600

    initial commit

---
+++ conditional-statement/conditional-statement/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conditional_statement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double score = 95;
+            char grade = 'F';
+
+            if(score >= 90)
+            {
+                grade = 'A';
+            }
+
+            if(score >= 80 && score < 90)
+            {
+                grade = 'B';
+            }
+
+            if (score >= 70 && score < 80)
+            {
+                grade = 'C';
+            }
+
+            if (score >= 60 && score < 70)
+            {
+                grade = 'D';
+            }
+
+            //if (score < 60 )
+            //{
+            //    grade = 'F';
+            //}
+            Console.WriteLine("***Using if-only****");
+            Console.WriteLine("Your grade is " + grade.ToString());
+
+            if(score >= 90)
+            {
+                grade = 'A';
+            }else if(score >= 80)
+            {
+                grade = 'B';
+            }
+            else if (score >= 70)
+            {
+                grade = 'C';
+            }
+            else if (score >= 60)
+            {
+                grade = 'D';
+            }else
+            {
+                grade = 'F';
+            }
+            Console.WriteLine("\r\n***Using if-elseif-else****");
+            Console.WriteLine("Your grade is " + grade.ToString());
+        }
+    }
+}
